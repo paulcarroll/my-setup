8b5b13d0a9a1a587bc6dd3e0eb09ebc5d48a31bd
Paul Carroll <paul.carroll@gmail.com>
2019-01-29 08:14:29 +1100

    add dotnet sdk

--- bin/ubuntu-18.04-setup.sh
+++ bin/ubuntu-18.04-setup.sh
@@ -33,6 +33,17 @@ npm install -g yarn
 
 
 # ----
+# dotnet sdk
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo add-apt-repository universe
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.2
+
+
+# ----
 # Micro editor
 curl https://getmic.ro | bash
 
